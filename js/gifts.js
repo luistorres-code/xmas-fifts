04b5fdaa66cbb1289ad615eb2d51922d664d2189
Luis <luiss.torress@yahoo.com>
2024-12-06 15:09:47 -0600

    Termina la pantalla de bienvenida e inicio

---
+++ js/gifts.js
@@ -0,0 +1,44 @@
+const GIFT_INFORMATION = {
+	1: {
+		name: "Cargador Inalámbrico",
+		description: "Estación de carga MagSage para iPhone, Airpods y Apple Watch",
+		price: 583.99,
+		image: "https://m.media-amazon.com/images/I/619i8DIQy+L._AC_SL1500_.jpg",
+		link: "https://a.co/d/1tqIayK",
+	},
+	2: {
+		name: "Sudadera",
+		description: "Sudadera con capucha y bolsillo canguro de Hollister",
+		price: 787.58,
+		image: "https://img.hollisterco.com/is/image/anf/KIC_322-4176-00210-200_model1.jpg",
+		link: "https://www.hollisterco.com/shop/wd-es/p/sudadera-holgada-con-capucha-y-logo-58565821?categoryId=12572&faceout=model&seq=16",
+	},
+	3: {
+		name: "Bateria externa",
+		description: "Batería externa de 20,000mAh con 4 puertos de carga",
+		price: 588.55,
+		image: "https://m.media-amazon.com/images/I/71ManMP7pZL._AC_SL1500_.jpg",
+		link: "https://a.co/d/alWVTil",
+	},
+	4: {
+		name: "Kindle",
+		description: "Lector de libros electrónicos con luz frontal ajustable",
+		price: 2099.0,
+		image: "https://m.media-amazon.com/images/I/71w-4uiDzoL._AC_SL1500_.jpg",
+		link: "https://a.co/d/g7CsPbQ",
+	},
+	5: {
+		name: "Cápsulas de café Nespresso Vertuo",
+		description: "Cápsulas de café Nespresso Vertuo, mis favoritas son: Il Caffè, Diavolito, Melozio e Intenso",
+		price: 0.0,
+		image: "https://m.media-amazon.com/images/I/61K9nWSTB6L.jpg",
+		link: "https://www.nespresso.com/mx/es/order/capsules/vertuo",
+	},
+	6: {
+		name: "Sudadera nacota de Calvin Klein",
+		description: "Sudadera con capucha y bolsillo canguro de Calvin Klein",
+		price: 2499.0,
+		image: "https://calvinkleinmx.vteximg.com.br/arquivos/ids/506646-1764.1000000000001-1923/J30J320805P41-PLANO.jpg",
+		link: "https://www.calvinklein.mx/sudadera-calvin-klein-monologo-mujer-gris-j30j320805-p41/p",
+	},
+};
---
+++ js/nodes.js
@@ -0,0 +1,7 @@
+const $ = (id) => document.getElementById(id);
+
+// Main Pages
+const welcomeMain = $("welcomeMain");
+
+//Welcome Page
+const welcomeForm = $("welcomeForm");
