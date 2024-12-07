5e856a32bc4d8ebd6dca3a93075c301aa99c675d
Luis <luiss.torress@yahoo.com>
2024-12-06 18:07:16 -0600

    UI: Se finaliza UI, se inicia desarrollo de comportamiento

--- js/nodes.js
+++ js/nodes.js
@@ -2,6 +2,20 @@ const $ = (id) => document.getElementById(id);
 
 // Main Pages
 const welcomeMain = $("welcomeMain");
+const mainPage = $("mainPage");
+const viewPage = $("viewPage");
 
 //Welcome Page
 const welcomeForm = $("welcomeForm");
+
+// Main Page
+const giftsContainer = $("giftsContainer");
+
+// View Page
+const navbarContainer = $("navbarContainer");
+const photoGift = $("photoGift");
+const priceGift = $("priceGift");
+const nameGift = $("nameGift");
+const descriptionGift = $("descriptionGift");
+const linkGift = $("linkGift");
+const otherPlace = $("otherPlace");
